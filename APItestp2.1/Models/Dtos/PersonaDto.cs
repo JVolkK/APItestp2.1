e8608f5ba8912511b7fb6505d0036b93fab3438f
JVolkK <122408877+JVolkK@users.noreply.github.com>
2024-04-15 22:31:51 -0300

    Cree la carpeta Dtos y repositorio dentro de la carpetea Models, configure la concexion con la base de datos en el Program.cs y en el appsettings.json. Cree la carpeta Context

--- APItestp2.1/Models/Dtos/PersonaDto.cs
+++ APItestp2.1/Models/Dtos/PersonaDto.cs
@@ -2,6 +2,7 @@
 {
     public class PersonaDto
     {
+        //Nombre, Dni, FechadeNacimiento
         public string Nombre { get; set; }
         public string Dni { get; set; }
         public DateTime FechaDeNacimiento { get; set; }
